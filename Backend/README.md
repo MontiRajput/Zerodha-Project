5e7a581c87f0e50c4d11352ae07e9777e0377102
Monti Rajput <montirajput@delta.in>
2024-10-15 15:25:45 +0530

    h

---
+++ Backend/README.md
@@ -0,0 +1,2 @@
+# Zerodha-Backend
+![Upload File](https://img.shields.io/badge/Upload_File-grey?style=for-the-badge)
--- Frontend/src/LandingPage/Login.jsx
+++ Frontend/src/LandingPage/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://zerodha-bac.vercel.app/login",
+        "https://zerodha-backend-frb9.onrender.com/login",
         {
           ...inputValue,
         },
