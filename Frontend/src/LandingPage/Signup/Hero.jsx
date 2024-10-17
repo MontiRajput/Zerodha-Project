223d16f2bd4df3ea9a48116c2c856f87a10c49a6
Monti Rajput <montirajput@delta.in>
2024-10-17 08:53:58 +0530

    login

--- Frontend/src/LandingPage/Signup/Hero.jsx
+++ Frontend/src/LandingPage/Signup/Hero.jsx
@@ -65,13 +65,13 @@ function Signup() {
   };
 
   return (
-    <div className="Container m-5 p-5">
+    <div className="Container m-md-5 p-5">
       <div className="row p-md-5">
-        <div className="col-md-6 ">
-          <img src="Media/img/signup.png" style={{ maxHeight: "90%" }} />
+        <div className="col col-md-6 mt-3">
+          <img src="Media/img/signup.png" style={{ width: "90%" }} />
         </div>
-        <div className="col"></div>
-        <div className="col-md-5">
+
+        <div className=" col col-md-5">
           <h2>Signup now</h2>
           <p className="text-muted">Or track your existing application</p>
           <Box
